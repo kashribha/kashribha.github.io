f5a37af0ebcf6b5a1747a270af4f5cac705886e2
Kashribha <shrinidhibhagavath11@gmail.com>
2026-06-07 01:39:13 +0530

    Cleanup: removed python generator scripts and moved old_backup to parent directory

--- update_git.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "Completely overhauled portfolio to modern single-page Solarized Light theme with CSS animations"
-# Not pushing automatically, just committing locally
